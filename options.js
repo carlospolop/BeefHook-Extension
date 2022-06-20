c2340754661124443b9a81341cb39348000dc79a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-20 12:39:29 +0800

    Delete options.js

--- options.js
+++
@@ -1,6 +0,0 @@
-function saveOptions(e) {
-  chrome.storage.local.set({
-    url: document.querySelector("#url").value
-  });
-}
-document.querySelector("form").addEventListener("submit", saveOptions);
